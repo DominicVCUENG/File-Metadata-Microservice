58537be646a8b5fd1bac8e5c977eef8d5dc8c31c
Dominic Mason <masond4@vcu.edu>
2024-06-06 20:20:55 -0400

    added endpoint

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -11,8 +12,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer();
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  const metadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+
+  res.json(metadata);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
